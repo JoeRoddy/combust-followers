9d11eda5170a2ab01a0e6cce117d71c0cbf5d0a9
Joe Roddy <mrjoeroddy@gmail.com>
2018-06-22 13:57:44 -0400

    unused vars

--- components/FollowersList.jsx
+++ components/FollowersList.jsx
@@ -2,23 +2,20 @@ import React from "react";
 import { observer } from "mobx-react";
 
 import followerStore from "../../stores/FollowerStore";
-import userStore from "../../stores/UserStore";
 import UserList from "../users/UserList";
 
-const FollowersList = observer(
-  ({ displayFollowers, displayFollowing, history }) => {
-    const users = displayFollowing
-      ? followerStore.usersBeingFollowed
-      : followerStore.followers;
+const FollowersList = observer(({ displayFollowing, history }) => {
+  const users = displayFollowing
+    ? followerStore.usersBeingFollowed
+    : followerStore.followers;
 
-    return (
-      <UserList
-        title="Friends"
-        users={users}
-        onUserClicked={user => history.push("/profile/" + user.id)}
-      />
-    );
-  }
-);
+  return (
+    <UserList
+      title="Friends"
+      users={users}
+      onUserClicked={user => history.push("/profile/" + user.id)}
+    />
+  );
+});
 
 export default FollowersList;
